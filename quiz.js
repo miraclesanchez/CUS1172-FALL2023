57085f8d74501ef19c9e52a7744a980847f7b994
Miracle Sanchez <118776480+miraclesanchez@users.noreply.github.com>
2023-11-17 00:59:13 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -7,37 +7,42 @@ const model = {
                 "QuestionType": "multiple_choice",
                 "Question": "What type of programming language is Java?",
                 "Answers" : ["Imperative","Functional","Logical","Object-Oriented"],
-                "Correct Answer": "Object-Oriented"
+                "Correct_Answer": "Object-Oriented",
+                "Explanation": "With Java, you build your code around objects instead of logic."
             },
             "Question2" :{
                 "QuestionType": "picture",
                 "Question": "Which image shows the correct way to code the main method used in Java?",
                 "Answers" : ["java_correct.png", "java_wrong.png"],
-                "Correct Answer": "java_correct.png"
+                "Correct_Answer": "java_correct.png",
+                "Explanation": "In java the syntax for writing methods is (accesss_modifier) (static) (return_type) (method_name) (parameters). In the main method the acccess_modifier is publix, return type is void because nothing has to be returned, the method name is main, and the parameters are a string array of arguments which is what you write as your code"
             },
             "Question3" :{
                 "QuestionType": "multiple_choice",
                 "Question": "Which is the correct way to declare a constant variable in Java?",
                 "Answers" : ["constant int num =4;","var int num =4;", "let int num =4;", "final int num =4;"],
-                "Correct Answer": "final int num =4;"
+                "Correct_Answer": "final int num =4;",
+                "Explanation": "Var and let are used to declare variables in Javascript. Constant is typically seen in shorthand as 'const' in JS and C++. In Java we use 'final' to declare a variable that cannot be modified."
+
             },
             "Question4" :{
                 "QuestionType": "multiple_choice",
                 "Question": "What would you use the keyword 'super' for in Java?",
                 "Answers" : ["To call the superclass constructor", "To refer to a variable", "To create a class", "To create a method"],
-                "Correct Answer": "To call the superclass constructor"
+                "Correct_Answer": "To call the superclass constructor",
+                "Explanation": "The superclass refers to the class from which a subclass inherits. To inherit from a class means you can use their methods or access thier fields. In order to access from a super class we use the 'super' keyword.",
             },
             "Question5" :{
                 "QuestionType": "multiple_choice",
                 "Question": "What would you use the keyword 'super' for in Java?",
                 "Answers" : ["To call the superclass constructor", "To refer to a variable", "To create a class", "To create a method"],
-                "Correct Answer": "To call the superclass constructor"
+                "Correct_Answer": "To call the superclass constructor"
             },
             // "Question5" :{
             //     "QuestionType": "narrative",
             //     "Question": "Lets say you are working on a Java program and you have a class and its constructor (an object of the class). You also have variables declared before the constructor which are used within the parameters of the constructor. What keyword do you use to initialize the variable to equal the parameter?",
             //     "Answers" : ["that","this"],
-            //     "Correct Answer": "this"
+            //     "Correct_Answer": "this"
             // },
        }, 
         "QUIZ2" :{
@@ -46,31 +51,31 @@ const model = {
                 "Question Type" : "multiple choice",
                 "Question": "What is 2+3",
                 "Answers" : [2,4,5,6],
-                "Correct Answer": "4"
+                "Correct_Answer": "4"
             },
             "Question2" :{
                 "Question Type" : "multiple choice",
                 "Question": "What is 2+3",
                 "Answers" : [2,4,5,6],
-                "Correct Answer": "4"
+                "Correct_Answer": "4"
             },
             "Question3" :{
                 "Question Type" : "multiple choice",
                 "Question": "What is 2+2",
                 "Answers" : [2,4,5,6],
-                "Correct Answer": "4"
+                "Correct_Answer": "4"
             },
             "Question4" :{
                 "Question Type" : "multiple choice",
                 "Question": "What is 2+2",
                 "Answers" : [2,4,5,6],
-                "Correct Answer": "4"
+                "Correct_Answer": "4"
             },
             "Question5" :{
                 "Question Type" : "multiple choice",
                 "Question": "What is 2+2",
                 "Answers" : [2,4,5,6],
-                "Correct Answer": "4"
+                "Correct_Answer": "4"
             }
        }  
     }
@@ -87,10 +92,11 @@ const appState = {
     currentQuestion: "",
     question_type: "",
     good_feedback: "Brilliant !!",
-    final_grade: 0,
+    final_grade: "",
 };
 
 document.addEventListener('DOMContentLoaded',function(){
+        render_view(model, "#initial");
         //updates label and clears the name fields
         document.querySelector('#form').onsubmit=function(){
 
@@ -180,7 +186,7 @@ const handle_answer = (event) =>{
         console.log(appState.userAnswer);
         
            
-            const correctAnswer = appState.currentQuestion [`Correct Answer`];
+            const correctAnswer = appState.currentQuestion [`Correct_Answer`];
             console.log('correct: ' + correctAnswer);
             const type = appState.currentQuestion [`Question Type`];
 
@@ -196,7 +202,7 @@ const handle_answer = (event) =>{
                                 console.log(appState.currentQuestionIndex);
                                 create_question(appState.currentQuestionIndex); 
                                 }else if(appState.currentQuestionIndex === appState.totalQuestions){
-                                    appState.final_grade = appState.questions_correct / appState.totalQuestions;
+                                    appState.final_grade = appState.questions_correct +"/" + appState.totalQuestions;
                                     console.log(appState.final_grade);
                                     render_view(model, "#final_results");
                                     console.log("done")
@@ -208,7 +214,20 @@ const handle_answer = (event) =>{
                 console.log('Questions Incorrect: '+appState.questions_wrong);
                 displayBadMessage();
 
-            }
+                    document.querySelector('#got_it').onclick = () =>{
+                    if(appState.currentQuestionIndex < appState.totalQuestions) {
+                    appState.currentQuestionIndex +=1;
+                    console.log(appState.currentQuestionIndex);
+                    create_question(appState.currentQuestionIndex); 
+                    }else if(appState.currentQuestionIndex === appState.totalQuestions){
+                        appState.final_grade = appState.questions_correct +"/" + appState.totalQuestions;
+                        console.log(appState.final_grade);
+                        render_view(model, "#final_results");
+                        console.log("done")
+                    }
+                }
+
+            } 
             
         
     }
@@ -264,7 +283,7 @@ const handle_narrative = (event) =>{
 //         appState.currentQuestion = model.results[appState.currentQuiz][`Question${appState.currentQuestionIndex}`];
 //         console.log(appState.userAnswer);
 
-//         const correctAnswer = appState.currentQuestion [`Correct Answer`];
+//         const correctAnswer = appState.currentQuestion [`Correct_Answer`];
 //             console.log('correct: ' + correctAnswer);
 //             const type = appState.currentQuestion [`Question Type`];
 
